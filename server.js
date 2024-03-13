1584af12b3fbcd72b468284a8f2fb3fc0fbc2c6a
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-03-13 14:53:54 +0530

    5th Commit

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const { schema } = require('./schema');
 const { resolvers } = require('./resolvers');
+const { validateQueryDepth, createComplexityLimitRule } = require('graphql-validation-complexity');
+const jwt = require('jsonwebtoken');
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers: resolvers,
+  validationRules: [validateQueryDepth(5), createComplexityLimitRule(1000)],
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
+    try {
+      const user = jwt.verify(token, 'secret-key');
+      return { user };
+    } catch (error) {
+      return {};
+    }
+  },
 });
 
 server.listen({ port: 4001 }).then(({ url }) => {
